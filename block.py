c322be6bdc0f009c1ac6c8611e993166a3890806
Luiz Angioletti <luizfelipeasoares@gmail.com>
2021-09-29 22:09:53 -0400

    class Block in a different file

---
+++ block.py
@@ -0,0 +1,18 @@
+import time
+
+class Block:
+    def __init__(self):
+        self.index = None
+        self.transactions = []
+        self.create_timestamp = time.time() 
+        self.previous_hash = None
+        self.nonce = ''
+        self.hash = ''
+
+    def add_transac(transac):
+        if len(transac) < 11:
+            self.transactions.append(transac)
+            return True
+        else:
+            return False
+
