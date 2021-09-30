f33fb52b18c1186a7de3e833a96237c96d71775a
Luiz Angioletti <luizfelipeasoares@gmail.com>
2021-09-30 12:11:07 -0400

    Wallet is the entity that holds products and their balances.

---
+++ wallet.py
@@ -0,0 +1,34 @@
+class Wallet:
+    '''
+    Object that retains balances for users.
+    Each new user instantiates a new wallet from scratch and adds balance to it.
+    That balance will be use to validate that the user has enough resources to
+    operate in transactions.
+
+    arguments:  none
+    returns  :  wallet object with products and their respective balances.
+
+    To manipulate the products in a wallet, specific methods have to be used
+    '''
+    def __init__(self):
+        self.products = dict()
+
+
+    def add_resource(self, product, amount):
+        if product.id in [x for x in self.products.keys()]:
+            self.products[product.id] += amount
+        else:
+            self.products[product.id] = amount
+
+
+    def subtract_resource(self, product, amount):
+        if product.id not in [x for x in self.products.keys()] \
+                or self.products[product.id] < amount:
+            print("Insufficient balance")
+            return False
+        else: self.products[product.id] -= amount
+
+    def __repr__(self):
+        header = ['Total balance for this wallet:']
+        products = ["{prd} : {qty}".format(prd=x[:6], qty=y) for x,y in self.products.items()]
+        return '\n'.join(header + products)
