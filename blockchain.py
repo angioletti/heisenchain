5f7206ecf855f695188fb9d05aa307abb83fae1a
Luiz Angioletti <luizfelipeasoares@gmail.com>
2021-10-01 16:40:18 -0400

    computation of hash and validation of block got moved to miner

--- blockchain.py
+++ blockchain.py
@@ -40,24 +40,6 @@ class Blockchain:
         genesis_block.hash = '0'*64
         self.chain.append(genesis_block)
 
-    def compute_hash(self, block):
-        bstring = json.dumps(block.__dict__, sort_keys=True)
-        bhash = sha256(bstring.encode()).hexdigest()
-        return bhash
-
-    def validate_block(self, block):
-        '''
-        Validates a block by computing a valid hash and, through that,
-        providing a proof of work type of consensus.
-        There is no protocol implemented yet to accommodate multiple miners.
-        Returns a tuple with valid hash and its nonce.
-        '''
-        block.nonce = 0
-        valid_hash = ''
-        while not valid_hash.startswith('0' * self.difficulty):
-            valid_hash = self.compute_hash()
-            block.nonce += 1
-        return valid_hash, block.nonce - 1
 
     def is_valid_hash(self, block):
         '''
@@ -107,10 +89,3 @@ class Blockchain:
         self.pool_transaction.append(transac)
 #
 # 
-#         def validate_coverage(sender, product, quantity):
-#             self.sender = sender
-#             if trtype != 'create' and self.sender.products[product] >= quantity:
-#                 return True
-#             else:
-#                 print("Insufficient funds to cover the transaction.")
-#                 return False
