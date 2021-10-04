7a7d01a2cfadff4ad657ed29b26bccc686d514ad
Luiz Angioletti <luizfelipeasoares@gmail.com>
2021-10-04 18:59:08 -0400

    changed the name of the transactions list to be clearer

--- block.py
+++ block.py
@@ -3,12 +3,17 @@ import time
 class Block:
     def __init__(self):
         self.index = None
-        self.transactions = []
+        self.transaction_list = []
         self.create_timestamp = time.time()
         self.previous_hash = None
         self.nonce = ''
         self.hash = ''
 
-    def add_transac(transac):
-        self.transactions.append(transac)
+    def __repr__(self):
+        # FIXIT: this repr doesn't make much sense
+        prn = [self.index, self.transaction_list, ]
+        return (str(self.index) + str(self.transaction_list) + str(self.create_timestamp)
+                     + str(self.previous_hash) + str(self.nonce))
 
+    def add_transac(self, transac):
+        self.transaction_list.append(transac)
