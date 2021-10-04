e10360072df7ca303d47391aec6ee4074ee768ce
Luiz Angioletti <luizfelipeasoares@gmail.com>
2021-10-04 18:58:15 -0400

    changed name of the products dictionary to be more clear on what it is

--- wallet.py
+++ wallet.py
@@ -14,7 +14,7 @@ class Wallet:
     To manipulate the products in a wallet, specific methods have to be used
     '''
     def __init__(self, blockchain):
-        self.products = dict()
+        self.product_dict = dict()
         self.wallet_id = self.wallet_id()
         self.blockchain = blockchain
 
@@ -23,23 +23,30 @@ class Wallet:
         return wallet_id
 
     def add_resource(self, product, amount):
-        if product.id in [x for x in self.products.keys()]:
-            self.products[product.id] += amount
+        if product.id in [x for x in self.product_dict.keys()]:
+            self.product_dict[product.id] += amount
         else:
-            self.products[product.id] = amount
+            self.product_dict[product.id] = amount
 
 
     def subtract_resource(self, product, amount):
-        if product.id not in [x for x in self.products.keys()] \
-                or self.products[product.id] < amount:
+        if product.id not in [x for x in self.product_dict.keys()] \
+                or self.product_dict[product.id] < amount:
             print("Insufficient balance")
             return False
-        else: self.products[product.id] -= amount
+        else: self.product_dict[product.id] -= amount
 
     def __repr__(self):
         header = ['Total balance for this wallet:']
-        if len(self.products) == 0:
+        if len(self.product_dict) == 0:
             return self.wallet_id[:4] + ": No funds!\n"
         else:
-            products = ["{prd} : {qty}".format(prd=x[:6], qty=y) for x,y in self.products.items()]
+            products = ["{prd} : {qty}".format(prd=x[:6], qty=y) for x,y in self.product_dict.items()]
             return '\n'.join(header + products + ['\n'])
+
+    def check_balance(self, product):
+        if product in self.product_dict.keys():
+            return product.dict[product]
+        else:
+            return 0
+
