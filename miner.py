7709998a7cf3373c502d0f1e8fc670a64d1404b4
Luiz Angioletti <luizfelipeasoares@gmail.com>
2021-10-04 21:33:32 -0400

    rename variables for clarity / fixed operate_transac() / fixed validate_block()

--- miner.py
+++ miner.py
@@ -1,54 +1,68 @@
 import blockchain as bch
 import time
+import json
+import hashlib as hl
 import block as blk
 import transaction as trn
 import product as prd
 
 class Miner:
-    def __init__(self, blockchain, block):
+    def __init__(self, blockchain):
         self.bchain = blockchain
-        self.block = block
+        self.block = blk.Block()
 
-    def bundle_pending_transactions(self, self.block):
+    def bundle_pending_transactions(self):
         for index, i in enumerate(self.bchain.pool_transaction):
             self.operate_transac(i, index)
-            block.add_transac(i)
-            bchain.pool_transaction.pop(index)
+            print("Processing transaction " + str(index) + ": " + str(i))
+        self.bchain.pool_transaction = []
 
     def validate_coverage(self,transac):
-        if transac.trtype != 'create' and transac.sender.products[product] >= quantity:
+        if transac.sender.check_balance(transac.product) >= transac.quantity:
             return True
         else:
             print("Insufficient funds to cover the transaction.")
             return False
 
     def operate_transac(self,transac,index):
-        transac.coverage = validate_coverage(transac)
-        if transac.coverage == True:
-            if transac.trtype = 'create':
-                transac.receipt.add_resources(transac.product, transac.quantity)
-            else:
-                transac.sender.subtract_resources(transac.product, transac.quantity)
-                transac.receipt.add_resources(transac.product, transac.quantity)
+        if transac.trtype == 'create':
+            transac.recepient.add_resource(transac.product, transac.quantity)
+            self.block.add_transac(transac)
+            self.bchain.pool_transaction.pop(index)
+        elif transac.trtype == 'remove':
+            transac.coverage = self.validate_coverage(transac)
+            if transac.coverage == True:
+                transac.recepient.subtract_resource(transac.product, transac.quantity)
+                self.block.add_transac(transac)
+                self.bchain.pool_transaction.pop(index)
+        elif transac.trtype == 'transfer':
+            transac.coverage = self.validate_coverage(transac)
+            if transac.coverage == True:
+                transac.recepient.add_resource(transac.product, transac.quantity)
+                transac.sender.subtract_resource(transac.product, transac.quantity)
+                self.block.add_transac(transac)
+                self.bchain.pool_transaction.pop(index)
         else:
-            bchain.pool_transaction.pop(index)
-            bchain.pool_rejected_transac.append(transac)
+            self.bchain.pool_transaction.pop(index)
+            self.bchain.pool_rejected_transaction.append(transac)
 
-    def compute_hash(self, block):
-        bstring = json.dumps(block.__dict__, sort_keys=True)
-        bhash = sha256(bstring.encode()).hexdigest()
+    def compute_hash(self):
+        bstring = repr(self.block)
+        bhash = hl.sha256(bstring.encode()).hexdigest()
         return bhash
 
-    def validate_block(self, block):
+    def validate_block(self):
         '''
         Validates a block by computing a valid hash and, through that,
         providing a proof of work type of consensus.
         There is no protocol implemented yet to accommodate multiple miners.
         Returns a tuple with valid hash and its nonce.
         '''
-        block.nonce = 0
+        self.block.nonce = 0
         valid_hash = ''
-        while not valid_hash.startswith('0' * self.difficulty):
+        while not valid_hash.startswith('0' * self.bchain.difficulty):
             valid_hash = self.compute_hash()
-            block.nonce += 1
-        return valid_hash, block.nonce - 1
+            self.block.nonce += 1
+        self.block.hash, self.block.nonce = (valid_hash, self.block.nonce - 1)
+        print("Block {} validated and sent to blockchain for inclusion".format(self.block.hash[:8]))
+        self.bchain.pool_unconfirmed_block.append(self.block)
